983765d60e11f8e74b240a4587bc26d9f53fb29c
Eddo091 <eddishu0@gmail.com>
2020-07-31 23:46:29 -0600

    Actualizacion

--- app/src/main/java/com/example/calculadorabasica/MainActivity.java
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.calculadorabasica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Calcular (View vista){
+        TextView tempval=(TextView)findViewById(R.id.txtnum1);
+        double num1= Double.parseDouble( tempval.getText().toString());
+
+        tempval=(TextView)findViewById(R.id.txtnum2);
+        double num2= Double.parseDouble((tempval.getText().toString()));
+
+        double respuesta= num1 + num2;
+       tempval=(TextView)findViewById(R.id.lblresp);
+       tempval.setText("Respuesta: " + respuesta);
     }
 }
