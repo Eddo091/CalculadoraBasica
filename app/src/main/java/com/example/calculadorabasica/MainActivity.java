63429e2e1d37b61e60aca0317441035dfb703fe9
Eddo091 <eddishu0@gmail.com>
2020-08-01 01:04:04 -0600

    RadioGroups, RadioButtons, Formulas basicas

--- app/src/main/java/com/example/calculadorabasica/MainActivity.java
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -15,15 +17,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void Calcular (View vista){
+    public void Procesar (View vista){
+        try{
+        RadioGroup optOperaciones=(RadioGroup)findViewById(R.id.optOperaciones);
         TextView tempval=(TextView)findViewById(R.id.txtnum1);
         double num1= Double.parseDouble( tempval.getText().toString());
 
         tempval=(TextView)findViewById(R.id.txtnum2);
         double num2= Double.parseDouble((tempval.getText().toString()));
+        double respuesta=0;
+        switch (optOperaciones.getCheckedRadioButtonId()) {
+            case R.id.rbnSuma:
+                respuesta = num1 + num2;
+                break;
+            case R.id.rbnResta:
+                respuesta = num1 - num2;
+                break;
+            case R.id.rbnMulti:
+                respuesta = num1 * num2;
+                break;
+            case R.id.rbnDivicion:
+                respuesta = num1 / num2;
+                break;
+            case R.id.rbnexpo:
+                respuesta= Math.pow(num1, num2);
+                break;
+            case R.id.rbnPorcen:
+                respuesta = (num1 / num2) * num1;
+                break;
+        }
+            tempval=(TextView)findViewById(R.id.lblresp);
+            tempval.setText("Respuesta: " + respuesta);
+        } catch (Exception err){
+            Toast.makeText(getApplicationContext(), "Porfavor ingrese valores", Toast.LENGTH_LONG).show();
 
-        double respuesta= num1 + num2;
-       tempval=(TextView)findViewById(R.id.lblresp);
-       tempval.setText("Respuesta: " + respuesta);
+        }
     }
+
+
 }
